077addb36ff93f50c7b816a53f049be2cd62f048
liutao <taoliuh@gmail.com>
2015-08-28 21:31:44 +0800

    add problem description.

--- search2dmatrix-ii/src/Solution.java
+++ search2dmatrix-ii/src/Solution.java
@@ -1,4 +1,15 @@
 /**
+ * Search a 2D Matrix II
+ * Write an efficient algorithm that searches for a value in an m x n matrix, return the occurrence of it.
+ *
+ * This matrix has the following properties:
+ *
+ * Integers in each row are sorted from left to right.
+ *
+ * Integers in each column are sorted from up to bottom.
+ *
+ * No duplicate integers in each row or column.
+ *
  * Created by liutao on 8/28/15.
  */
 public class Solution {
