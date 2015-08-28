745446a2d197114d26091b4472112a682de3cb99
liutao <taoliuh@gmail.com>
2015-08-28 20:25:50 +0800

    added search 2d matrix.

---
+++ search2dmatrix/src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by liutao on 8/28/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {{1, 3, 5, 7},{10, 11, 16, 20}, {23, 30, 34, 50}};
+        Solution solution = new Solution();
+        boolean result = solution.searchMatrix(matrix, 50);
+        System.out.printf("result is %b", result);
+    }
+}
